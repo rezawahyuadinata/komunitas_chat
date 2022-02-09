3b5ef1e224c7c856a881da38bc64157eefe9848b
Reza Wahyu Adinata <adinatarezawahyu@gmail.com>
2022-02-09 11:54:48 +0700

    perbaikan dari esting

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,11 @@ class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return Container();
   }
